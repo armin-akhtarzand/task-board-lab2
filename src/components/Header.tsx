13ee0ef04a5ffc53aa49c58cdf55125d6bc0a9b8
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-09-08 11:06:44 +0200

    change header css from regular to tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
     return(
-        <header className="header">
+        <header className="text-center p-4 border-2 border-gray-400">
             <p>Webbutveckling</p>
-            <h1>Team Task Board</h1>
+            <h1 className="font-bold m-3 text-3xl">Team Task Board</h1>
             <p>Detta är den andra laborationen i kursen Webbutveckling</p>
         </header>
     );
