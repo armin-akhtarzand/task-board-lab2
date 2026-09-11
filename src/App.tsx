6d4a31af1f5b44819da379e74195396e8afc5fae
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-09-11 17:13:38 +0200

    feat: create taskboard component for task columns

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Column from "./components/Column";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import TaskCard from "./components/TaskCard";
+import TaskBoard from "./components/TaskBoard";
 import type { NewTask, Task } from "./types/Task";
 import TaskForm from "./components/TaskForm";
 import { useEffect, useState } from "react";
@@ -49,62 +49,11 @@ const App = () => {
       console.error(error);
     }
   };
-
-  const todo: Task[] = tasks.filter((task) => task.status === "todo");
-  const doing: Task[] = tasks.filter((task) => task.status === "doing");
-  const done: Task[] = tasks.filter((task) => task.status === "done");
-
   return (
     <div className="min-h-screen bg-orange-50 text-gray-900">
       <Header></Header>
       <main className="space-y-12 py-8">
-        <div className="mx-auto grid max-w-7xl grid-cols-1 gap-8 px-4 lg:grid-cols-3">
-          <Column title="Todo">
-            <div className="flex flex-wrap justify-center gap-8 px-5 py-2.5">
-              {todo.map((task) => (
-                <TaskCard
-                  id={task.id}
-                  key={task.id}
-                  title={task.title}
-                  description={task.description}
-                  assignee={task.assignee}
-                  priority={task.priority}
-                  category={task.category}
-                ></TaskCard>
-              ))}
-            </div>
-          </Column>
-          <Column title="Doing">
-            <div className="flex flex-wrap justify-center gap-8 px-5 py-2.5">
-              {doing.map((task) => (
-                <TaskCard
-                  key={task.id}
-                  id={task.id}
-                  title={task.title}
-                  description={task.description}
-                  assignee={task.assignee}
-                  priority={task.priority}
-                  category={task.category}
-                ></TaskCard>
-              ))}
-            </div>
-          </Column>
-          <Column title="Done">
-            <div className="flex flex-wrap justify-center gap-8 px-5 py-2.5">
-              {done.map((task) => (
-                <TaskCard
-                  key={task.id}
-                  id={task.id}
-                  title={task.title}
-                  description={task.description}
-                  assignee={task.assignee}
-                  priority={task.priority}
-                  category={task.category}
-                ></TaskCard>
-              ))}
-            </div>
-          </Column>
-        </div>
+        <TaskBoard tasks={tasks}></TaskBoard>
         <Column title="Formulär">
           <div className="px-4">
             <TaskForm onAddTask={addTask}></TaskForm>
