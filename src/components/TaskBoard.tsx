29ea341d6fffc5330a9ed90b5c4633659bf091fa
armin-akhtarzand <armin.akhtarzand@outlook.com>
2026-09-11 17:14:41 +0200

    prettier code format

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,67 +1,66 @@
-import type { Task } from "../types/Task"
+import type { Task } from "../types/Task";
 import Column from "./Column";
 import TaskCard from "./TaskCard";
 
-type TaskBoardProps ={
-    tasks: Task[];
-}
-
-const TaskBoard = ({tasks}: TaskBoardProps) => {
-        const todo: Task[] = tasks.filter((task) => task.status === "todo");
-        const doing: Task[] = tasks.filter((task) => task.status === "doing");
-        const done: Task[] = tasks.filter((task) => task.status === "done");
-
-    return (
-  <div className="mx-auto grid max-w-7xl grid-cols-1 gap-8 px-4 lg:grid-cols-3">
-    <Column title="Todo">
-      <div className="flex flex-wrap justify-center gap-8 px-5 py-2.5">
-        {todo.map((task) => (
-          <TaskCard
-            key={task.id}
-            id={task.id}
-            title={task.title}
-            description={task.description}
-            assignee={task.assignee}
-            priority={task.priority}
-            category={task.category}
-          />
-        ))}
-      </div>
-    </Column>
-
-    <Column title="Doing">
-      <div className="flex flex-wrap justify-center gap-8 px-5 py-2.5">
-        {doing.map((task) => (
-          <TaskCard
-            key={task.id}
-            id={task.id}
-            title={task.title}
-            description={task.description}
-            assignee={task.assignee}
-            priority={task.priority}
-            category={task.category}
-          />
-        ))}
-      </div>
-    </Column>
-
-    <Column title="Done">
-      <div className="flex flex-wrap justify-center gap-8 px-5 py-2.5">
-        {done.map((task) => (
-          <TaskCard
-            key={task.id}
-            id={task.id}
-            title={task.title}
-            description={task.description}
-            assignee={task.assignee}
-            priority={task.priority}
-            category={task.category}
-          />
-        ))}
-      </div>
-    </Column>
-  </div>
-);
-
+type TaskBoardProps = {
+  tasks: Task[];
 };
-export default TaskBoard
+
+const TaskBoard = ({ tasks }: TaskBoardProps) => {
+  const todo: Task[] = tasks.filter((task) => task.status === "todo");
+  const doing: Task[] = tasks.filter((task) => task.status === "doing");
+  const done: Task[] = tasks.filter((task) => task.status === "done");
+
+  return (
+    <div className="mx-auto grid max-w-7xl grid-cols-1 gap-8 px-4 lg:grid-cols-3">
+      <Column title="Todo">
+        <div className="flex flex-wrap justify-center gap-8 px-5 py-2.5">
+          {todo.map((task) => (
+            <TaskCard
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              description={task.description}
+              assignee={task.assignee}
+              priority={task.priority}
+              category={task.category}
+            />
+          ))}
+        </div>
+      </Column>
+
+      <Column title="Doing">
+        <div className="flex flex-wrap justify-center gap-8 px-5 py-2.5">
+          {doing.map((task) => (
+            <TaskCard
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              description={task.description}
+              assignee={task.assignee}
+              priority={task.priority}
+              category={task.category}
+            />
+          ))}
+        </div>
+      </Column>
+
+      <Column title="Done">
+        <div className="flex flex-wrap justify-center gap-8 px-5 py-2.5">
+          {done.map((task) => (
+            <TaskCard
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              description={task.description}
+              assignee={task.assignee}
+              priority={task.priority}
+              category={task.category}
+            />
+          ))}
+        </div>
+      </Column>
+    </div>
+  );
+};
+export default TaskBoard;
